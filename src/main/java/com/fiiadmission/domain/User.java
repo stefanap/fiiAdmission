648c1a5650f34caa0e067134e673306d1e57ce3b
nicolaeabacioaiei <nicolae.abacioaiei@gmail.com>
2018-05-16 07:33:08 +0300

    database configuration from application.properties PopulateDb class to populate the db

--- src/main/java/com/fiiadmission/domain/User.java
+++ src/main/java/com/fiiadmission/domain/User.java
@@ -17,7 +17,7 @@ public class User {
     }
     
     @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
